d38d9276c1af9760ff00f8875c8f1f5865c4276a
Rafael Reiter <mail@zonk.at>
2013-02-11 21:51:15 +0100

    class doc

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -23,6 +23,8 @@ import sys, os
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
 
+autoclass_content = "both"
+
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 
