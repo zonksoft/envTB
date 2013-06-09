020bd106d5951962349773c18bd2d17e4debad96
Rafael Reiter <mail@zonk.at>
2013-06-09 23:14:07 +0200

    fourier.py code style

--- envtb/utility/fourier.py
+++ envtb/utility/fourier.py
@@ -11,30 +11,30 @@ class FourierTransform:
         self.transformed_data = utilities.LinearInterpolationNOGrid(
             self.__calculate_fourier_transform(self.original_data),
             reciprocal_latticevecs)
-        
+
     def __calculate_fourier_transform(self, original_data):
         return numpy.fft.fftn(original_data)
-        
+
     @staticmethod
     def calculate_fourier_grid(latticevecs, data_shape):
-        reciprocal_latticevecs=numpy.array([numpy.cross(latticevecs[1], latticevecs[2]),
+        reciprocal_latticevecs = numpy.array([numpy.cross(latticevecs[1], latticevecs[2]),
                                                numpy.cross(latticevecs[2], latticevecs[0]),
                                                numpy.cross(latticevecs[0], latticevecs[1])
                                                ])
         reciprocal_latticevecs *= 2 * numpy.pi / numpy.linalg.det(latticevecs)
-        
+
         gridsize = data_shape
-        
-        return reciprocal_latticevecs/data_shape, numpy.array([[[numpy.dot([i,j,k], reciprocal_latticevecs) 
-                  for k in numpy.arange(0, 1, 1./gridsize[2])] 
-                 for j in numpy.arange(0, 1, 1./gridsize[1])]
-                for i in numpy.arange(0, 1, 1./gridsize[0])])
-        
+
+        return reciprocal_latticevecs / data_shape, numpy.array([[[numpy.dot([i, j, k], reciprocal_latticevecs)
+                  for k in numpy.arange(0, 1, 1. / gridsize[2])]
+                 for j in numpy.arange(0, 1, 1. / gridsize[1])]
+                for i in numpy.arange(0, 1, 1. / gridsize[0])])
+
     def plot(self, ax):
         data_to_plot = None
         im = ax.imshow(data_to_plot)
         return im
-    
+
 
 class RealSpaceWaveFunctionFourierTransform:
     def __init__(self, real_space_orbitals):
@@ -46,9 +46,9 @@ class RealSpaceWaveFunctionFourierTransform:
         for i, orb in real_space_orbitals.orbitals.iteritems():
             # XXX: if orb is given as analytic function: think of good gridsize
             self.fourier_transformations[i] = orb.fourier_transform()
-        
+
     # XXX: write function to go from wave function in vector to array
-        
+
     def fourier_transform(self, latticevecs, wave_function):
         """
         Coefficients of the wave function on a nonorthogonal grid (in a
@@ -58,16 +58,16 @@ class RealSpaceWaveFunctionFourierTransform:
         wave_function = numpy.array(wave_function)
         transformed_wave_function = numpy.fft.fftn(wave_function)
         transformed_lattice_vectors = FourierTransform.calculate_fourier_grid(latticevecs, wave_function.shape)
-        
+
         transformed_coefficients = {}
         for orbnr, orb_transformation in self.fourier_transformations.iteritems():
             # XXX: if you transform several wave functions, you can save this
             transformed_orb_values_on_grid = numpy.array([[[
-                        orb_transformation(point) for point in row] 
+                        orb_transformation(point) for point in row]
                     for row in slice]
                 for slice in transformed_lattice_vectors])
-                
+
             transformed_coefficients[orbnr] = \
-                transformed_wave_function*transformed_orb_values_on_grid
-        
+                transformed_wave_function * transformed_orb_values_on_grid
+
         return transformed_coefficients
