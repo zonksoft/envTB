12ae121d88a37a9d43b7d381fa18a956eebd36f5
Rafael Reiter <mail@zonk.at>
2013-06-22 21:08:17 +0200

    Added ZigzagGNRHelper

--- envtb/utility/fourier.py
+++ envtb/utility/fourier.py
@@ -1,5 +1,6 @@
 import numpy
 import envtb.quantumcapacitance.utilities as utilities
+from envtb.wannier90.w90hamiltonian import Hamiltonian
 
 class FourierTransform:
     def __init__(self, latticevecs, data_on_grid, shape=None, axes=None):
@@ -123,3 +124,96 @@ class RealSpaceWaveFunctionFourierTransform:
             transformed_wave_functions[orbnr] = transformed_wave_function
 
         return transformed_coefficients, transformed_wave_functions
+        
+# XXX: Class is badly structured and documented      
+class ZigzagGNRHelper:
+    def __init__(self, nnfile, height, length, paddingx=0, paddingy=0):
+        """
+        height: height of zGNR in 4-atom basiscells
+        length: number of slices
+        paddingx, paddingy: number of padding orbitals in x and y direction
+        nnfile: Nearest neighbour file with geometry
+        """
+        self.height, self.length = height,length
+        self.paddingx, self.paddingy = paddingx, paddingy
+        self.nnfile = nnfile
+        
+    def __create_hamiltonian(self, nnfile, height, length):
+    
+        unitcells=self.height/2
+        ham = Hamiltonian.from_nth_nn_list(nnfile)
+        
+        ham2 = ham.create_supercell_hamiltonian(
+            [[0, 0, 0], [1, 0, 0]],
+            [[1, -1, 0], [1, 1, 0], [0, 0, 1]])
+        
+        ham3 = ham2.create_supercell_hamiltonian(
+            [[0, i, 0] for i in range(unitcells)],
+            [[1, 0, 0], [0, unitcells, 0], [0, 0, 1]])
+        
+        ham4 = ham3.create_modified_hamiltonian(
+            ham3.drop_dimension_from_cell_list(1))
+        
+        ham5 = ham4.create_supercell_hamiltonian([[i, 0, 0] for i in range(
+            length)], [[length, 0, 0], [0, 1, 0], [0, 0, 1]])
+        
+        return ham, ham5
+
+
+    def __create_transformations(self, ham):
+        a1, a2, a3 = ham.latticevectors()
+        b1 = 8*a1
+        b2 = 4*a2
+        b3 = a3
+        
+        c1=b1
+        c2=(a1-a2)*4
+        c3=a3
+
+        vecs=numpy.vstack((b1,b2,b3))
+        gr_vecs=numpy.vstack((a1,a2,a3))
+        invvecs = numpy.linalg.inv(vecs)
+        invgr_vecs = numpy.linalg.inv(gr_vecs)
+        
+        return vecs, gr_vecs, invvecs, invgr_vecs, a1, a2, a3, b1, b2, b3, c1, c2, c3
+
+    def split_sublattices(self, vec, nrorbs):
+        return numpy.array([[vec[i] for i in range(start,len(vec),nrorbs)] for start in range(nrorbs)])
+
+    def __shift_vector(self, vec, b1, b2, c2, invvecs):
+        transformed = numpy.dot(vec,invvecs)
+        transformed_right = numpy.dot(vec-c2,invvecs)
+        if transformed[1] < 0:
+            if transformed_right[0] < 0:
+                return vec + b1/2 + b2
+            else:
+                return vec - b1/2 + b2
+        else:
+            return vec
+        
+    def resort_nanoribbon(self):    
+        ham, ham5 = self.__create_hamiltonian(self.nnfile, self.height, self.length)
+        vecs, gr_vecs, invvecs, invgr_vecs, a1, a2, a3, b1, b2, b3, c1, c2, c3 = self.__create_transformations(ham)
+          
+        orbpos = numpy.array(ham5.orbitalpositions())
+        
+        coords = self.split_sublattices(orbpos,2)[0]
+        shiftvecs = numpy.array([self.__shift_vector(vec, b1, b2, c2, invvecs) for vec in coords])
+        enumeration=numpy.array([range(len(shiftvecs))]).transpose()
+        l=numpy.round(numpy.hstack((numpy.dot(shiftvecs,invgr_vecs),enumeration))*3)/3
+        ind=numpy.lexsort((l[:,0],l[:,1]))    
+        return l[ind][:,3]
+
+    def pad_nanoribbon_vector_to_periodic(self, vec):
+        splitvecs=numpy.split(vec,len(vec)/(4*self.height-2))
+        splitvecs=[numpy.append(splitvec,0) for splitvec in splitvecs]
+        splitvecs=[numpy.insert(splitvec,0,0) for splitvec in splitvecs]
+        return numpy.array(splitvecs).flatten()
+
+    def pad_vector(self, vec):
+        splitvecs=numpy.split(vec,len(vec)/(4*self.height))
+        splitvecs=[numpy.append(splitvec,[0]*self.paddingy) for splitvec in splitvecs]
+        return numpy.concatenate((numpy.array(splitvecs).flatten(),numpy.zeros((self.paddingy+self.height)*self.paddingx)))
+
+    def vector_to_grid(self, vec):
+        return numpy.reshape(vec,(-1,self.height*4))
