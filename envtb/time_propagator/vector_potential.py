534185c1f80e614f7a8bfcd9d037b6810b919557
Larisa Chizhova <larisa@test>
2013-06-23 14:23:22 +0200

    adding direction and CEP to vector potential

--- envtb/time_propagator/vector_potential.py
+++ envtb/time_propagator/vector_potential.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pylab as plt
 
 light_speed = 3 * 10**(8)
+print 'Agrh!'
 
 class VectorPotential:
     
@@ -18,66 +19,73 @@ class VectorPotential:
         pass
     
     def plot_electric_field(self):
-        tarr = np.linspace(-0.1 / self.frequency, 6 * np.pi / self.frequency, 100)
+        tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
         plt.subplot(1,2,1)
         Ex = [self.get_electric_field(t)[0] for t in tarr]
-        plt.plot(tarr, Ex)
+        plt.plot(tarr, Ex, label = r'$E_x$')
         plt.subplot(1,2,2)
         Ey = [self.get_electric_field(t)[1] for t in tarr]
-        plt.plot(tarr, Ey)
+        plt.plot(tarr, Ey, label = r'$E_y$')
     
     def plot_pulse(self):
-        tarr = np.linspace(-0.1 / self.frequency, 6 * np.pi / self.frequency, 100)
+        tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
         plt.subplot(1,2,1)
         Ax = [self.__call__(t)[0] for t in tarr]
-        plt.plot(tarr, Ax)
+        plt.plot(tarr, Ax, label=r'$A_x$')
+        plt.xlabel(r'$t, s$')
         plt.subplot(1,2,2)
         Ay = [self.__call__(t)[1] for t in tarr]
-        plt.plot(tarr, Ay)
+        plt.plot(tarr, Ay, label=r'$A_y$')
+        plt.xlabel(r'$t, s$')
     
     def __call__(self, t):
         pass
     
 class VectorPotentialWave(VectorPotential):
     
-    def __init__(self, amplitude_E0, frequency):
+    def __init__(self, amplitude_E0, frequency, direction=[1.0,0.0]):
         
         """
-        amplitude_E0: is the peak filed strength
+        amplitude_E0: is a maximum field strength
         """
-        
         self.amplitude= amplitude_E0
         self.frequency = frequency
+        norm = np.sqrt(direction[0]**2+direction[1]**2)
+        self.direction = np.array(direction)/norm
         
     def __call__(self, t):
-        
-        return [self.amplitude * light_speed / self.frequency * np.sin(self.frequency * t), 0]
+        VecPot = self.amplitude * light_speed / self.frequency * np.sin(self.frequency * t)
+        return [VecPot * self.direction[0], VecPot * self.direction[1]]
        
     
 class LP_FlatTopPulse(VectorPotential):
     
-    def __init__(self, amplitude_E0, frequency):
+    def __init__(self, amplitude_E0, frequency, direction=[1.0,0.0]):
         
         """
-        amplitude_E0: is the peak filed strength
+        amplitude_E0: is the maximum field strength
         """
-        
         self.amplitude= amplitude_E0
         self.frequency = frequency
+        norm = np.sqrt(direction[0]**2+direction[1]**2)
+        self.direction = np.array(direction)/norm
             
     def __call__(self, t):
         
         if t < 0:
             return [0, 0]
         elif t >= 0 and t < np.pi/self.frequency:
-            return [self.amplitude * light_speed * t / np.pi * np.sin(self.frequency * t), 0]
+            VecPot = self.amplitude * light_speed * t / np.pi * np.sin(self.frequency * t)
+            return [VecPot * self.direction[0], VecPot * self.direction[1]]
         else:
-            return [self.amplitude * light_speed / self.frequency * np.sin(self.frequency * t), 0]  
+            VecPot = self.amplitude * light_speed / self.frequency * np.sin(self.frequency * t)
+            return [VecPot * self.direction[0], VecPot * self.direction[1]]
 
     
 class LP_SinSqEnvelopePulse(VectorPotential):
     
-    def __init__(self, amplitude_E0, frequency, Nc = 1): 
+    def __init__(self, amplitude_E0, frequency, Nc=1, cep=0.0, direction=[1.0,0.0]): 
+        
         """
         This class contains linearly polarized light filed described
         by the vector potential with a sin^2 envelope 
@@ -93,14 +101,17 @@ class LP_SinSqEnvelopePulse(VectorPotential):
         self.frequency = frequency
         self.Nc = Nc
         self.pulse_duration = 2. * self.Nc * np.pi / self.frequency
-        
+        norm = np.sqrt(direction[0]**2 + direction[1]**2)
+        self.direction = np.array(direction) / norm
+        self.CEP = cep
     
     def __call__(self, t):
         
         if t >= 0 and t < self.pulse_duration:
-            return [-self.amplitude * light_speed / self.frequency *\
+            VecPot = -self.amplitude * light_speed / self.frequency *\
                     np.sin(self.frequency * t / 2./self.Nc)**2 *\
-                    np.sin(self.frequency * t), 0]
+                    np.sin(self.frequency * t + self.CEP)
+            return [VecPot * self.direction[0], VecPot * self.direction[1]]
         else:
             return [0.,0]
         
@@ -108,10 +119,11 @@ class LP_SinSqEnvelopePulse(VectorPotential):
         tarr = np.linspace(0., self.pulse_duration, 100)
         plt.subplot(1,2,1)
         A = [self.amplitude * light_speed / self.frequency *\
-                np.sin(self.frequency * t / 2./self.Nc)**2 for t in tarr]
+                np.sin(self.frequency * t / 2./self.Nc)**2 * self.direction[0] for t in tarr]
         plt.plot(tarr, A, '--')
         plt.subplot(1,2,2)
-        A = [0 for t in tarr]
+        A = [self.amplitude * light_speed / self.frequency *\
+                np.sin(self.frequency * t / 2./self.Nc)**2 * self.direction[1] for t in tarr]
         plt.plot(tarr, A, '--')
         
                    
