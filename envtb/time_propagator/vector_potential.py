d4b5aa8a03d352833e48d0cb2746ab55c2bcea50
Larisa Chizhova <larisa@test>
2013-08-28 16:50:15 +0200

    fixing dimensionality bug in vec_pot. Finishing implementation of 3 vec_pots: SqSin, Gaus, FlatTop, adding fourier transform of pot

--- envtb/time_propagator/vector_potential.py
+++ envtb/time_propagator/vector_potential.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pylab as plt
 import math
 
-light_speed = 3 * 10**(8)
+#light_speed = 3 * 10**(8)
 
 class VectorPotential:
     
@@ -10,37 +10,114 @@ class VectorPotential:
         self.frequency = None
         self.amplitude = None
         self.pulse_duration = None
-            
+    
+    def __envelope(self, t):
+        pass
+
     def get_electric_field(self, t):
         dt = 0.01 * 10**(-16)
-        return [-(self(t + dt)[0] - self(1.*t)[0]) / dt / light_speed, -(self(t + dt)[1] - self(1.*t)[1]) / dt / light_speed]
+        return [-(self(t + dt)[0] - self(1.*t)[0]) / dt, -(self(t + dt)[1] - self(1.*t)[1]) / dt]
     
     def get_magnetic_filed(self):
         pass
     
-    def plot_electric_field(self):
+    def plot_electric_field(self, **kwrds):
         tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
         plt.subplot(1,2,1)
         Ex = [self.get_electric_field(t)[0] for t in tarr]
-        plt.plot(tarr, Ex, label = r'$E_x$')
+        plt.plot(tarr, Ex, label = r'$E_x$', **kwrds)
         plt.subplot(1,2,2)
         Ey = [self.get_electric_field(t)[1] for t in tarr]
-        plt.plot(tarr, Ey, label = r'$E_y$')
+        plt.plot(tarr, Ey, label = r'$E_y$', **kwrds)
     
-    def plot_vector_potential(self):
-        tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
+    def plot_vector_potential(self, trange=None, **kwrds):
+        if trange is None:
+            tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
+        else:
+            tarr = trange
         plt.subplot(1,2,1)
         Ax = [self.__call__(t)[0] for t in tarr]
-        plt.plot(tarr, Ax, label=r'$A_x$')
-        plt.xlabel(r'$t, s$')
+        plt.plot(tarr, Ax, label=r'$A_x$', **kwrds)
+        plt.xlabel(r'$t, s$', fontsize=24)
+        plt.ylim(1.1*min(Ax), 1.1 * max(Ax))
         plt.subplot(1,2,2)
         Ay = [self.__call__(t)[1] for t in tarr]
-        plt.plot(tarr, Ay, label=r'$A_y$')
-        plt.xlabel(r'$t, s$')
+        plt.plot(tarr, Ay, label=r'$A_y$', **kwrds)
+        plt.xlabel(r'$t, s$', fontsize=24)
+        plt.ylim(1.1 * min(Ay), 1.1 * max(Ay))
     
-    def __call__(self, t):
-        pass
+    def make_fourier_transform(self, tarr):
+        
+        Ax = [self.__call__(t)[0] for t in tarr]
+        Ay = [self.__call__(t)[1] for t in tarr]
+
+        Axfft = np.fft.fft(np.array(Ax))
+        Ayfft = np.fft.fft(np.array(Ay))
+        return Axfft, Ayfft
+
+    def plot_fourier_transform(self, trange=None, scale='log', **kwrds):
+        '''
+         scale = 'log' logarithmic scale
+         scale = 'lin' linear scale
+        '''
+        
+        if trange is None:
+            tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
+        else:
+            tarr = trange
+
+        Axfft, Ayfft = self.make_fourier_transform(tarr)
+        #energy = 2. * np.pi / trange * 4.1357 * 10**(-15)
+        
+        n = tarr.size
+        time_step = tarr[1]-tarr[0]
+
+        energy = np.fft.fftfreq(n, d=time_step) * 4.1357 * 10**(-15) 
+        
+        max_en = max(energy)
+        for i in xrange(0, len(energy)):
+           if energy[i]<0:
+              energy[i] += 2.*max_en
+
+        plt.subplot(1,2,1)        
+        if scale=='lin':
+           plt.plot(energy, abs(Axfft),label='Ax_fft', **kwrds)
+        elif scale=='log':
+           try:
+               plt.semilogy(energy, abs(Axfft), label='Ax_fft', **kwrds)
+           except:
+               pass
+        plt.ylabel('Ax_fft')
+        plt.xlabel('energy (eV)')
+        
+        plt.subplot(1,2,2)        
+        if scale=='lin':
+           plt.plot(energy, abs(Ayfft),label='Ay_fft', **kwrds)
+        elif scale=='log':
+           try:
+               plt.semilogy(energy, abs(Ayfft), label='Ay_fft', **kwrds)
+           except:
+               pass
+        plt.ylabel('Ay_fft')
+        plt.xlabel('energy (eV)')
     
+    def plot_envelope(self, trange=None):
+        
+        if trange is None:
+            tarr = np.linspace(-0.1 / self.frequency, 7 * np.pi / self.frequency, 100)
+        else:
+            tarr = trange
+        
+        plt.subplot(1,2,1)
+        A = [self.amplitude / self.frequency *\
+             self.direction[0] * self.envelope(t) for t in tarr]
+        plt.plot(tarr, A, '--')
+        plt.subplot(1,2,2)
+        A = [self.amplitude / self.frequency *\
+             self.direction[1] * self.envelope(t) for t in tarr]
+        plt.plot(tarr, A, '--')
+        
+
 class VectorPotentialWave(VectorPotential):
     
     def __init__(self, amplitude_E0, frequency, direction=[1.0,0.0], polarization='lin'):
@@ -54,13 +131,13 @@ class VectorPotentialWave(VectorPotential):
         self.direction = np.array(direction)/norm
         
     def __call__(self, t):
-        VecPot = self.amplitude * light_speed / self.frequency * np.sin(self.frequency * t)
+        VecPot = self.amplitude / self.frequency * np.sin(self.frequency * t)
         return [VecPot * self.direction[0], VecPot * self.direction[1]]
        
     
 class FlatTopPulse(VectorPotential):
     
-    def __init__(self, amplitude_E0, frequency, direction=[1.0,0.0], polarization='lin'):
+    def __init__(self, amplitude_E0, frequency, Tramp=None, duration=1.0, direction=[1.0,0.0], polarization='lin'):
         
         """
         amplitude_E0: is the maximum field strength
@@ -69,18 +146,35 @@ class FlatTopPulse(VectorPotential):
         self.frequency = frequency
         norm = np.sqrt(direction[0]**2+direction[1]**2)
         self.direction = np.array(direction)/norm
+        
+        if Tramp is None:
+           self.Tramp = np.pi/self.frequency
+        else:
+           self.Tramp = Tramp
+        print self.Tramp
+        self.pulse_duration = duration
+
+    def envelope(self, t):
+        
+        if t < 0 or t > self.pulse_duration + 2.*self.Tramp:
+            return 0.0
+        elif t >= 0 and t <= self.Tramp:
             
+            return t / self.Tramp
+        elif t > self.pulse_duration + self.Tramp:
+            return 2 + (self.pulse_duration - t) / self.Tramp 
+        else:
+            return 1.0
+    
+       
     def __call__(self, t):
         
-        if t < 0:
-            return [0, 0]
-        elif t >= 0 and t < np.pi/self.frequency:
-            VecPot = self.amplitude * light_speed * t / np.pi * np.sin(self.frequency * t)
-            return [VecPot * self.direction[0], VecPot * self.direction[1]]
-        else:
-            VecPot = self.amplitude * light_speed / self.frequency * np.sin(self.frequency * t)
-            return [VecPot * self.direction[0], VecPot * self.direction[1]]
+        env = self.envelope(t)
+        VecPot = self.amplitude / self.frequency * np.sin(self.frequency * t) * env
+        return [VecPot * self.direction[0], VecPot * self.direction[1]]
+    
 
+# end class FlatTopPulse
     
 class SinSqEnvelopePulse(VectorPotential):
     
@@ -96,7 +190,6 @@ class SinSqEnvelopePulse(VectorPotential):
         
         Nc: number of cycles
         """
-        
         self.amplitude= amplitude_E0
         self.frequency = frequency
         self.Nc = Nc
@@ -105,32 +198,26 @@ class SinSqEnvelopePulse(VectorPotential):
         self.direction = np.array(direction) / norm
         self.CEP = cep
     
-    def __call__(self, t):
+    def envelope(self, t):
         
         if t >= 0 and t < self.pulse_duration:
-            VecPot = -self.amplitude * light_speed / self.frequency *\
-                    np.sin(self.frequency * t / 2./self.Nc)**2 *\
-                    np.sin(self.frequency * t + self.CEP)
-            return [VecPot * self.direction[0], VecPot * self.direction[1]]
+            return  np.sin(self.frequency * t / 2./self.Nc)**2
         else:
-            return [0.,0]
+           return 0.0
+
+    def __call__(self, t):
         
-    def plot_envelope(self):
-        tarr = np.linspace(0., self.pulse_duration, 100)
-        plt.subplot(1,2,1)
-        A = [self.amplitude * light_speed / self.frequency *\
-                np.sin(self.frequency * t / 2./self.Nc)**2 * self.direction[0] for t in tarr]
-        plt.plot(tarr, A, '--')
-        plt.subplot(1,2,2)
-        A = [self.amplitude * light_speed / self.frequency *\
-                np.sin(self.frequency * t / 2./self.Nc)**2 * self.direction[1] for t in tarr]
-        plt.plot(tarr, A, '--')
+        env = self.envelope(t)
+         
+        VecPot = -self.amplitude / self.frequency *\
+                  np.sin(self.frequency * t + self.CEP) * env
+        return [VecPot * self.direction[0], VecPot * self.direction[1]]
         
-# end class LP_SinSqEnvelopePulse
+# end class SinSqEnvelopePulse
 
 class GaussianEnvelopePulse(VectorPotential):
     
-    def __init__(self, amplitude_E0, frequency, t0=None, Nc=1, cep=0.0, direction=[1.0,0.0], polarization='lin'):
+    def __init__(self, amplitude_E0, frequency, t0=None, tc=None, Nc=1, cep=0.0, direction=[1.0,0.0], polarization='lin'):
         
         """
         This class contains vector potential of a laser field
@@ -142,39 +229,33 @@ class GaussianEnvelopePulse(VectorPotential):
         
         t0: standard deviation of gaussian; FWHM = 1.177 * t0
         t0 = 2. * Nc / self.frequency
+
+        tc: center of the Gaussian
+        tc = 1.8 * (np.pi * Nc / self.frequency)
         """
         
         self.amplitude= amplitude_E0
         self.frequency = frequency
         self.pulse_duration = 2. * Nc * np.pi / self.frequency
         if t0 is None:
-            self.t0 = 2. * Nc / self.frequency
+            self.t0 = np.pi / 2.0 * Nc / self.frequency
         else:
             self.t0 = t0
-        self.tc = np.pi * Nc / self.frequency
+        if tc is None:
+            self.tc = 1.2 * np.pi * Nc / self.frequency
+        else:
+            self.tc = tc
         norm = np.sqrt(direction[0]**2 + direction[1]**2)
         self.direction = np.array(direction) / norm
         self.CEP = cep
     
+    def envelope(self, t):
+        return math.exp(-((t-self.tc)/self.t0)**2/0.7213)
+
     def __call__(self, t):
-        
-        if t >= 0 and t < self.pulse_duration:
-            VecPot = -self.amplitude * light_speed / self.frequency *\
-                    math.exp(-((t-self.tc)/self.t0)**2)*\
-                    math.sin(self.frequency * t + self.CEP)
-            return [VecPot * self.direction[0], VecPot * self.direction[1]]
-        else:
-            return [0.,0]
-        
-    def plot_envelope(self):
-        tarr = np.linspace(0., self.pulse_duration, 100)
-        plt.subplot(1,2,1)
-        A = [self.amplitude * light_speed / self.frequency *\
-                math.exp(-((t-self.tc)/self.t0)**2) * self.direction[0] for t in tarr]
-        plt.plot(tarr, A, '--')
-        plt.subplot(1,2,2)
-        A = [self.amplitude * light_speed / self.frequency *\
-                math.exp(-((t-self.tc)/self.t0)**2) * self.direction[1] for t in tarr]
-        plt.plot(tarr, A, '--')
+        env = self.envelope(t)
+        VecPot = -self.amplitude / self.frequency *\
+                 math.sin(self.frequency * t + self.CEP) * env
+        return [VecPot * self.direction[0], VecPot * self.direction[1]]
         
 # end class GaussianEnvelopePulse
